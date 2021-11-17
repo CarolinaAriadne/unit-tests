e81dd044007b4a31a186ad70bcd057060a04d3d5
Carolina Ariadne <c.ariadneadv@gmail.com>
2021-11-16 22:12:20 -0300

    Requisito-2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -15,25 +15,47 @@ const numbers = require('../src/numbers');
 */
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
-  it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
-      const recebeFunção = numbers(myArray)
-      
-      });
+  it('Verifica se retorna true se a função receber [1, 2, 3, 4, 5]', () => {    
 
+    //arrange/arranjo  => setar(colocar) as variáveis que serão testadas
+    let array = [1,2,3,4,5];
 
-  
-  //     it('Verifica se retorna false se a função receber [1, 2, `3`, 4, 5]', () => {
-  //     expect(numbers([1,2,'3',4,5].toBeFalsy()));
-  //     });  
+    //act => A funcionalidade que deve ser testada
+    let result = numbers(array); // chamada da função que retorna o resultado na variável return
+
+    //assert/afirma =>  Verificação do resultado da ação. Ou seja verifica se o resultado é o esperado
+    expect(result).toBe(true);
+            
+  });  
    
-  //     it('Verifica se retorna false se a função receber [1, `a`, 3]', () => {
-  //     expect(numbers([1,'a',3].toBeFalsy()));
-  //     })
-  
-  //     it('Verifica se retorna false se a função receber [``]', () => {
-  //     expect(numbers([''].toBeFalsy()));
-  //     });
+  it('Verifica se retorna false se a função receber [1, 2, `3`,4,5]', () => {
 
-  // });
+    let array = [1, 2, '3', 4, 5];
+    let result2 = numbers(array);
+    expect(result2).toBe(false);
+      
+   })
+       
+  it('Verifica se retorna false se a função receber [1,`a`, 3]', () => {
+
+    let array = [1,`a`, 3];
+    let result3 = numbers(array);
+    expect(result3).toBe(false);
+  }) 
+   
+  it('Verifica se retorna false se a função receber [``]', () => {
+
+    let array = [``];
+    let result4 = numbers(array);      
+    expect(result4).toBe(false);
+   });
 
 });
+
+
+
+
+// Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true - FEITO
+    // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false - FEITO
+    // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false - FEITO
+    // Escreva um teste em que a função recebe [' '] e retorna false - FEITO
