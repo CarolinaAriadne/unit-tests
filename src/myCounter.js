7aadc390ba979f17d019e85ba3cc263c4c369b88
Carolina Ariadne <c.ariadneadv@gmail.com>
2021-11-17 11:40:10 -0300

    Requisito-8

--- src/myCounter.js
+++ src/myCounter.js
@@ -21,10 +21,12 @@ const myCounter = () => {
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let counter1 = 2; counter1 <= 3; counter1 += 1) {
-      myArray.push(counter);
+      myArray.push(counter1);
     }
   }
   return myArray;
 };
 
+let resultado = myCounter();
+console.log(resultado);
 module.exports = myCounter;
